e3038ff7569193bbb71d08c5b4c5049823d78002
pikpokjeon <pikpokjeon@gmail.com>
2021-08-26 20:18:29 +0900

    숫자를 글자로 출력

--- variable.hs
+++ variable.hs
@@ -1,5 +1,5 @@
 main = do
     let name1 = "dudu"
     putStrLn (name1 ++ " wanna go dancing")
-    print 45
+    putStrLn (show 43) -- 숫자인데 글자로 표현해달라
    -- putStrLn ("he named himself " ++ name1 ++ " 3years ago.")
