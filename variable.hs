b32fe1d9801eaeb0c7223ebb4d623292d1319cc4
pikpokjeon <pikpokjeon@gmail.com>
2021-08-26 20:15:12 +0900

    주석처리

--- variable.hs
+++ variable.hs
@@ -1,4 +1,4 @@
 main = do
     let name1 = "dudu"
     putStrLn (name1 ++ " wanna go dancing")
-    putStrLn ("he named himself " ++ name1 ++ " 3years ago.")
+   -- putStrLn ("he named himself " ++ name1 ++ " 3years ago.")
