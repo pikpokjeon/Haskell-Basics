93bc37fb37dc0f70bbe8df9eb330fd67ddd33413
pikpokjeon <pikpokjeon@gmail.com>
2021-08-26 20:52:13 +0900

    char, double;

--- variable.hs
+++ variable.hs
@@ -1,9 +1,14 @@
 
 name :: String  -- 타입정의
 year :: Int 
+myLetter :: Char
+gpa :: Double
 
 year = 3 
 name = "pikpok"
+myLetter = 'A'
+
+gpa = 23.1
 
 main = do
     let name1 = "dudu" -- js에서 const처럼 불변 
@@ -11,3 +16,6 @@ main = do
     putStrLn ("he named himself " ++ name1 ++ show year ++" years ago.")
     putStrLn name
     print year
+    print myLetter
+    print gpa
+    print ( 1 + 1)
