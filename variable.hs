661ea0ec8b4135db3de66cbd3dce26a1fb603806
pikpokjeon <pikpokjeon@gmail.com>
2021-08-26 20:45:56 +0900

    string int 타입정의

--- variable.hs
+++ variable.hs
@@ -1,5 +1,13 @@
+
+name :: String  -- 타입정의
+year :: Int 
+
+year = 3 
+name = "pikpok"
+
 main = do
-    let name1 = "dudu" -- let키워드는 js에서 const처럼 불변 
-    let year = 3
-    putStrLn (name1 ++ " wanna go dancing")
+    let name1 = "dudu" -- js에서 const처럼 불변 
+    putStrLn (name ++ " wanna go dancing")
     putStrLn ("he named himself " ++ name1 ++ show year ++" years ago.")
+    putStrLn name
+    print year
